d1ef8833b34615c3063f02c1ccb7778115e8db51
stepforwardtech <jrstrayhorn@gmail.com>
2018-03-08 14:47:25 -0500

    Finished navigating with React Router Link

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,15 +1,17 @@
-import Link from './Link';
-import { connect } from 'react-redux';
-import { setVisibilityFilter } from '../actions';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const mapStateToLinkProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
-});
-const mapDispatchToLinkProps = (dispatch, ownProps) => ({
-  onClick() {
-    dispatch(setVisibilityFilter(ownProps.filter));
-  }
-});
-const FilterLink = connect(mapStateToLinkProps, mapDispatchToLinkProps)(Link);
+const FilterLink = ({ filter, children }) => (
+  <NavLink
+    exact
+    to={filter === 'all' ? '/' : `/${filter}`}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black'
+    }}
+  >
+    {children}
+  </NavLink>
+);
 
 export default FilterLink;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,11 @@ import FilterLink from './FilterLink';
 const Footer = () => (
   <div>
     <p>
-      Show: <FilterLink filter="SHOW_ALL">All</FilterLink>{' '}
-      <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>{' '}
-      <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
+      Show: <FilterLink filter="all">All</FilterLink>
+      {', '}
+      <FilterLink filter="active">Active</FilterLink>
+      {', '}
+      <FilterLink filter="completed">Completed</FilterLink>
     </p>
   </div>
 );
