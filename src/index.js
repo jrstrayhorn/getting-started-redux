de9b30e5ba75e3ac8374f383205fe2e3cf390c70
stepforwardtech <jrstrayhorn@gmail.com>
2018-03-08 10:43:33 -0500

    Finished persisting the state to the local storage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import throttle from 'lodash/throttle';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
-
 import App from './components/App';
 import todoApp from './reducers';
+import { loadState, saveState } from './localStorage';
 
-const persistedState = {
-  todos: [
-    {
-      id: 0,
-      text: 'Welcome Back!',
-      completed: false
-    }
-  ]
-};
-
+const persistedState = loadState();
 const store = createStore(todoApp, persistedState);
+
+store.subscribe(
+  throttle(() => {
+    saveState({ todos: store.getState().todos });
+  }, 1000)
+);
 
 ReactDOM.render(
   <Provider store={store}>
