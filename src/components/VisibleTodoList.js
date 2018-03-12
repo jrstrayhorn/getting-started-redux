05834048b6706c17851105fa66e8e60e833175af
stepforwardtech <jrstrayhorn@gmail.com>
2018-03-12 10:35:22 -0400

    Finished Fetching Data on Route Change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,17 +1,42 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TodoList from './TodoList';
 import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api';
 
-const mapStateToTodoListProps = (state, { match }) => ({
-  todos: getVisibleTodos(state, match.params.filter || 'all')
-});
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      );
+    }
+  }
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
+
+const mapStateToTodoListProps = (state, { match }) => {
+  const filter = match.params.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  };
+};
 
 // we are saying add onTodoClick to props as a method
 // that maps to toggleTodo action creator
-const VisibleTodoList = withRouter(
-  connect(mapStateToTodoListProps, { onTodoClick: toggleTodo })(TodoList)
+VisibleTodoList = withRouter(
+  connect(mapStateToTodoListProps, { onTodoClick: toggleTodo })(VisibleTodoList)
 );
 
 export default VisibleTodoList;
