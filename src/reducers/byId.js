f8b3760c9d002563eca474521da174600e6a1208
stepforwardtech <jrstrayhorn@gmail.com>
2018-03-13 11:01:45 -0400

    Finished Refactoring the Reducers

---
+++ src/reducers/byId.js
@@ -0,0 +1,19 @@
+// a lookup table or database of todos
+// key being the id
+// value the actually todo
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case 'RECEIVE_TODOS':
+      const nextState = { ...state };
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default byId;
+
+export const getTodo = (state, id) => state[id];
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,21 @@
 import { combineReducers } from 'redux';
-import todos, * as fromTodos from './todos';
+import byId, * as fromById from './byId';
+import createList, * as fromList from './createList';
 
-const todoApp = combineReducers({
-  todos
+const listByFilter = combineReducers({
+  all: createList('all'),
+  active: createList('active'),
+  completed: createList('completed')
 });
 
-export default todoApp;
+const todos = combineReducers({
+  byId,
+  listByFilter
+});
 
-// the state passed in is entire state of application
-// state used in fromTodos.getVisibleTodos is just todos from state
-export const getVisibleTodos = (state, filter) =>
-  fromTodos.getVisibleTodos(state.todos, filter);
+export default todos;
+
+export const getVisibleTodos = (state, filter) => {
+  const ids = fromList.getIds(state.listByFilter[filter]);
+  return ids.map(id => fromById.getTodo(state.byId, id));
+};
